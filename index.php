333cb61affc53e9e30cbca6e749bc3a916fc39ea
Roy Okuwaki <info@tinybeans.dev>
2023-04-19 15:34:11 +1200

    Updated index.php as sample

--- index.php
+++ index.php
@@ -7,15 +7,22 @@ header('Content-Type: application/json; charset=utf-8');
 
 $config = [
 //    // データを格納しているディレクトリのパス
-//    'dataDirPath' => null,
+//    'dataDirPath' => '',
 //    // キャッシュを保存するディレクトリのパス
-//    'cacheDirPath' => null,
+//    'cacheDirPath' => '',
+//    // マージする検索データのパス
+//    'margeItemsJsonPath' => [],
+//    // マージするメタデータのパス
+//    'margeMetaJsonPath' => [],
 //    // limit, offset の初期値を設定
 //    'limit'  => 20,
 //    'offset' => 0,
 //    // limit, offset にかかわらずすべての結果を返す場合は true をセット
 //    'return_all' => false,
-//    // フィルター検索を許可するパラメータ（ `search` `rand` `from` `to` は特殊なパラメータのためフィルターに利用できません）
+//    // 先頭のいくつかの item を抜き出して splicedItems に入れる。[$offset, $length] という 2要素を持つ配列で指定する。
+//    // URL パラメータで指定する場合は splice_o と splice_l で指定する
+//    'splice' => [], // 先頭の1つを抜き出す場合は [0, 1]
+//    // フィルター検索を許可するパラメータ（ `search` `rand` `from` `to` `operator` は特殊なパラメータのためフィルターに利用できません）
 //    'filters' => [
 //        'id' => 'eq',
 //        'title' => 'like',
@@ -31,6 +38,8 @@ $config = [
 //        'day' => 'eq',
 //        'datetime' => 'eq',
 //    ],
+//    // 初期値として与えるパラメータ
+//    'initParams' => [],
 //    // パラメータマッピング
 //    'paramMapping' => [
 //        'from' => 'datetime',
@@ -40,9 +49,9 @@ $config = [
 //    'sortBy' => 'datetime',
 //    'sortOrder' => 'descend', // 'ascend' or 'descend'
 //    // デバッグモードにする場合は true をセット
-//    'devMode' => true,
+//    'devMode' => false,
 //    // 処理時間を計測する場合は true をセット
-//    'returnTime' => true,
+//    'returnTime' => false,
 //    // 実行ファイルの URL を JSON に含める場合は true をセット
 //    'includeScriptUrl' => true,
 //    // リファラの URL を JSON に含める場合は true をセット
@@ -53,15 +62,19 @@ $config = [
 //    'viewPagesLimit' => 3, // 3, 5, 7, 9 ...
 ];
 
-/**
- * レスポンスを返す直前実行されます。
- *
- * @param $result レスポンスの配列です。
- * @return Array $result をカスタマイズして返却します。
- */
+//function modifyGetParams($params) {
+//    if (isset($params['search']) && $params['search']) {
+//        $params['search'] = '無鉄砲 ' . $params['search'];
+//    }
+//    return $params;
+//}
+
 //function beforeResponse($result) {
 //    $result['foo'] = true;
 //    return $result;
 //}
 
 $bpsearch = new BPSearch($config);
+//$bpsearch->addCallback('modifyGetParams', 'modifyGetParams');
+//$bpsearch->addCallback('beforeResponse', 'beforeResponse');
+$bpsearch->run();
